e60ed6d48910294c3d210c70c0756eea955b97cb
Gavrie Philipson <gavrie@philipson.co.il>
2020-04-28 15:37:20 +0300

    Improved error handling

--- unit-testing/src/bin/19_exercise1/main.rs
+++ unit-testing/src/bin/19_exercise1/main.rs
@@ -1,14 +1,96 @@
 use std::error::Error;
 
+//use indoc::indoc;
+use thiserror;
+use twoway;
+
+#[derive(thiserror::Error, Debug)]
+enum RespError {
+    #[error("not enough data: found {found}, expected {expected}")]
+    NotEnoughData {
+        expected: usize,
+        found: usize,
+    },
+
+    #[error("missing length")]
+    MissingLength,
+
+    #[error("invalid data")]
+    InvalidData,
+}
+
 fn resp_parse(data: &[u8]) -> Result<&[u8], Box<dyn Error>> {
-    match data {
-        [b'+', string.., b'\r', b'\n'] => Ok(string),
-        //[b'$', ..] => Ok(&data[1..]),
-        _ => Err("invalid data".into()),
+    match &data {
+        [b'+', data @ ..] => {
+            let (line, _rest) = split_line(data);
+            line.ok_or("missing end of line deliminator".into())
+        }
+        [b'$', data @ ..] => match split_line(data) {
+            (Some(length), data) => {
+                let length: usize = std::str::from_utf8(length)?.parse()?;
+                if data.len() < length + 2 {
+                    Err(RespError::NotEnoughData {
+                        expected: length + 2,
+                        found: data.len(),
+                    }
+                        .into())
+                } else {
+                    let data = &data[..length];
+                    Ok(data)
+                }
+            }
+            (None, _) => Err(RespError::MissingLength.into()),
+        },
+        _ => Err(RespError::InvalidData.into()),
     }
 }
 
+fn split_line(data: &[u8]) -> (Option<&[u8]>, &[u8]) {
+    twoway::find_bytes(data, b"\r\n")
+        .map(|i| {
+            let line = &data[..i];
+            let rest = &data[i + 2..];
+            (Some(line), rest)
+        })
+        .unwrap_or((None, data))
+}
+
 #[test]
-fn test_resp_parse() {
+fn test_resp_parse_simple() {
     assert_eq!(resp_parse(b"+hello\r\n").unwrap(), b"hello");
+    assert_eq!(resp_parse(b"+hel\r\nlo\r\n").unwrap(), b"hel");
+}
+
+#[test]
+fn test_resp_parse_bulk() {
+    assert_eq!(
+        resp_parse(b"$11\r\nhello world\r\n").unwrap(),
+        b"hello world",
+    );
+
+    assert_eq!(
+        resp_parse(b"$12\r\nhello\r\nworld\r\n").unwrap(),
+        b"hello\r\nworld"
+    );
+
+    // TODO: Figure out how to work best with the combination of dynamic and static errors.
+    // Also apply that in the Peta project (instead of the `failure` crate).
+    // Maybe add a wrapped Box<dyn Error> as one of the `enum` values?
+    match resp_parse(b"$") {
+        Ok(data) => panic!("expected an error, got: {:?}", data),
+        Err(e) => {
+            match *e.downcast::<RespError>().expect("not a RespError") {
+                RespError::MissingLength => {}
+                e => panic!("wrong error: {}", e),
+            }
+        }
+    }
+
+    // assert_eq!(resp_parse(b"$11").unwrap(), b"???");
+    // assert_eq!(resp_parse(b"$11\r\n").unwrap(), b"???");
+    // assert_eq!(resp_parse(indoc!(b"
+    //     $12
+    //     hello
+    //     world
+    //     ")), b"hello\r\nworld");
 }
